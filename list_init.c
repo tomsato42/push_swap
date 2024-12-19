925f8e6d863b4464f06aa00566934c28d64be222
kinakqna <tomokirenrakuyo@gmail.com>
2024-12-19 20:21:36 +0900

    fix list_init.c norm

--- list_init.c
+++ list_init.c
@@ -6,15 +6,16 @@
 /*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/19 19:13:37 by tomsato           #+#    #+#             */
-/*   Updated: 2024/12/19 19:44:36 by tomsato          ###   ########.fr       */
+/*   Updated: 2024/12/19 20:21:04 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_ring_head	*list_init()
+t_ring_head	*list_init(void)
 {
 	t_ring_head	*new_list;
+
 	new_list = (t_ring_head *)malloc(sizeof(t_ring_head));
 	if (!new_list)
 	{
