4c948510d8d206fc6f47c034dab2cd0a5a2344d6
kinakqna <tomokirenrakuyo@gmail.com>
2024-12-19 20:07:01 +0900

    add list_init.c file

---
+++ list_init.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   list_init.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 19:13:37 by tomsato           #+#    #+#             */
+/*   Updated: 2024/12/19 19:44:36 by tomsato          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_ring_head	*list_init()
+{
+	t_ring_head	*new_list;
+	new_list = (t_ring_head *)malloc(sizeof(t_ring_head));
+	if (!new_list)
+	{
+		PRINT_ERROR();
+		return (NULL);
+	}
+	new_list->head = NULL;
+	new_list->size = 0;
+	return (new_list);
+}
