3e59b2441a40bbb5a2dddfaed0674f441c4e81db
kinakqna <tomokirenrakuyo@gmail.com>
2024-12-19 19:08:02 +0900

    add Doubly Circular List

--- push_swap.h
+++ push_swap.h
@@ -6,10 +6,29 @@
 /*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 20:58:36 by tomsato           #+#    #+#             */
-/*   Updated: 2024/12/17 21:05:57 by tomsato          ###   ########.fr       */
+/*   Updated: 2024/12/19 18:39:19 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+#include <stddef.h>
+
+/*双方向循環リスト(Doubly Circular List)*/
+typedef struct s_ring
+{
+	int				value;
+	struct s_ring	*next;
+	struct s_ring	*prev;
+	
+}	t_ring;
+
+typedef struct s_ring_head
+{
+	t_ring	*head;
+	size_t	size;
+}	t_ring_head;
+
+
 #endif
