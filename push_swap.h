69de51229d748189b30650495f515b0f9fd4ac52
kinakqna <tomokirenrakuyo@gmail.com>
2024-12-19 20:08:11 +0900

    add list_init() prototype

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 20:58:36 by tomsato           #+#    #+#             */
-/*   Updated: 2024/12/19 18:39:19 by tomsato          ###   ########.fr       */
+/*   Updated: 2024/12/19 19:44:02 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 # define PUSH_SWAP_H
 
 #include <stddef.h>
+#include <stdlib.h>
+
+/* エラー出力マクロ */
+# define PRINT_ERROR() (write(2, "Error\n", 6))
 
 /*双方向循環リスト(Doubly Circular List)*/
 typedef struct s_ring
@@ -24,11 +28,14 @@ typedef struct s_ring
 	
 }	t_ring;
 
+/*双方向循環リストの先頭*/
 typedef struct s_ring_head
 {
 	t_ring	*head;
 	size_t	size;
 }	t_ring_head;
 
+/*リストのメモリアドレスを確保*/
+t_ring_head	*list_init(void);
 
 #endif
