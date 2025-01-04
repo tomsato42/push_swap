6c838e0de67cc635c0166aa48b492035cb2f8cc2
kinakqna <tomokirenrakuyo@gmail.com>
2025-01-04 17:40:02 +0900

    add func is_better_move

--- greedy_sort.c
+++ greedy_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   greedy_sort.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomsato <tomsato@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*   By: tomsato <tomsato@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 18:40:38 by tomsato           #+#    #+#             */
-/*   Updated: 2025/01/02 22:58:46 by tomsato          ###   ########.fr       */
+/*   Updated: 2025/01/04 17:37:14 by tomsato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,36 +24,57 @@ typedef struct s_best_move
 static long	ft_l_min(long a, long b)
 {
 	if (a < b)
-		return a;
+		return (a);
 	else
-		return b;
+		return (b);
 }
 
 static long	ft_l_abs(long num)
 {
 	if (num < 0)
-		return -num;
+		return (-num);
 	else
-		return num;
+		return (num);
 }
 
 static t_best_move	calc_score(long i, long j, long from_size, long dist_size)
 {
 	t_best_move	calc;
 
-	printf("if (i(%ld) < ft_l_abs(i(%ld) - from_size(%ld))(%ld))\n",i,i,from_size,ft_l_abs(i - from_size));
 	if (i < ft_l_abs(i - from_size))
 		calc.from = i;
 	else
 		calc.from = (i - from_size);
-	printf("if (j(%ld) < ft_l_abs(j(%ld) - dist_size(%ld))(%ld))\n",j,j,dist_size,ft_l_abs(j - dist_size));
 	if (j < ft_l_abs(j - dist_size))
 		calc.dist = j;
 	else
-		calc.dist = (j - dist_size);;
-	printf("calc.score = min(min(abs(%ld), abs(%ld)) + abs(%ld, - %ld), abs(%ld) + abs(%ld)));",calc.from,calc.dist,calc.from,calc.dist,calc.from,calc.dist);
-	calc.score = ft_l_min(ft_l_min(ft_l_abs(calc.from), ft_l_abs(calc.dist)) + ft_l_abs(calc.from - calc.dist), ft_l_abs(calc.from) + ft_l_abs(calc.dist));
+		calc.dist = (j - dist_size);
+	;
+	calc.score = ft_l_min(ft_l_min(ft_l_abs(calc.from), ft_l_abs(calc.dist))
+			+ ft_l_abs(calc.from - calc.dist), ft_l_abs(calc.from)
+			+ ft_l_abs(calc.dist));
 	return (calc);
+}
+
+static int	is_better_move(t_ring_head *from, t_ring_head *dist,
+		t_best_move best, t_best_move score)
+{
+	const int	node_value = from->head->value;
+
+	if (score.score < best.score)
+	{
+		if (dist->head->prev->value < node_value)
+		{
+			if (node_value < dist->head->next->value)
+				return (1);
+			if (dist->head->value < dist->head->prev->value)
+				return (1);
+		}
+		if (node_value < dist->head->value)
+			if (dist->head->prev->value < dist->head->value)
+				return (1);
+	}
+	return (0);
 }
 
 static t_best_move	find_best_move(t_ring_head *from, t_ring_head *dist)
@@ -68,21 +89,11 @@ static t_best_move	find_best_move(t_ring_head *from, t_ring_head *dist)
 	best.score = LONG_MAX;
 	while (i < (long)from->size)
 	{
-		printf("\n\ni = %ld\n",i);
 		while (j < (long)dist->size)
 		{
-			printf("\n\nj=%ld\n",j);
-			score = calc_score(i,j,(long)from->size,(long)dist->size);
-			printf("score:i=%ld,j=%ld,score=%ld\n",score.from,score.dist,score.score);
-			printf("from->head->value(%d) < dist->head->value(%d)\n",from->head->value,dist->head->value);
-			if (score.score < best.score
-				&& ((from->head->value < dist->head->value)
-					&& (dist->head->value < from->head->value)))
-			//ここの条件式をもっとちゃんと整理する
-			{
-				write(1,"yeah\n",5);
+			score = calc_score(i, j, (long)from->size, (long)dist->size);
+			if (is_better_move(from, dist, best, score))
 				best = score;
-			}
 			dist->head = dist->head->next;
 			j++;
 		}
@@ -93,7 +104,8 @@ static t_best_move	find_best_move(t_ring_head *from, t_ring_head *dist)
 	return (best);
 }
 
-// static void	apply_best_move(t_ring_head *from, t_best_move *dist, t_best_move min)
+// static void	apply_best_move(t_ring_head *from, t_best_move *dist,
+//		t_best_move min)
 // {
 // }
 
@@ -127,33 +139,38 @@ static t_best_move	find_best_move(t_ring_head *from, t_ring_head *dist)
 // 	move_head_to_min_value(a);
 // }
 
-
 #include <stdio.h>
 
-void print_list(t_ring_head *list) {
-	t_ring *ptr = list->head;
-	for (size_t i = 0; i < list->size; i++) {
+void	print_list(t_ring_head *list)
+{
+	t_ring	*ptr;
+
+	ptr = list->head;
+	for (size_t i = 0; i < list->size; i++)
+	{
 		printf("%d, ", ptr->value);
 		ptr = ptr->next;
 	}
 	printf("\n");
 }
 
-int main(int argc, char **argv) {
-	int *arr = is_input_enabled(argc, argv);
-	if (!arr)
-		return 1;
+int	main(int argc, char **argv)
+{
+	int			*arr;
 	t_best_move	best;
+	t_ring_head	*list;
+	t_ring_head	*list2;
 
-	t_ring_head *list = list_init();
+	arr = is_input_enabled(argc, argv);
+	if (!arr)
+		return (1);
+	list = list_init();
 	list_add_tail(list, arr, argc - 1);
-
-	t_ring_head *list2 = list_init();
-	//list_add_tail(list2, arr, argc - 1);
+	list2 = list_init();
+	// list_add_tail(list2, arr, argc - 1);
 	free(arr);
 	if (!list || !list2)
-		return 1;
-
+		return (1);
 	printf("Init list\n");
 	print_list(list);
 	print_list(list2);
@@ -163,16 +180,18 @@ int main(int argc, char **argv) {
 	// print_list(list2);
 	// printf("\n");
 	// list_swap(list, list2, 's');
-	//list_rotate(list,list2,1,'r');
-	for (int i = 0; i < 3; i++) {
+	// list_rotate(list,list2,1,'r');
+	for (int i = 0; i < 3; i++)
+	{
 		list_push(list, list2, 'b');
 	}
 	print_list(list);
 	print_list(list2);
 	printf("\n");
 	// printf("%zu,%zu\n",list->size,list2->size);
-	best = find_best_move(list,list2);
-	printf("\nresult : \ni = %ld\nj = %ld\nscore = %ld\n\n",best.from,best.dist,best.score);
+	best = find_best_move(list, list2);
+	printf("\nresult : \ni = %ld\nj = %ld\nscore = %ld\n\n", best.from,
+		best.dist, best.score);
 	print_list(list);
 	print_list(list2);
 	printf("\n");
@@ -196,7 +215,7 @@ int main(int argc, char **argv) {
 	// print_list(list);
 	// print_list(list2);
 	// printf("\n");
-	// list_free(list);
-	// list_free(list2);
-	return 0;
+	list_free(list);
+	list_free(list2);
+	return (0);
 }
